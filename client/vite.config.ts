d0a05052435a6ee6c8e200a80186003ceed52b99
Ayush Babbar <76393139+AyushBabbar@users.noreply.github.com>
2024-03-02 23:04:47 +0530

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,12 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
+server: {
+host: true,
+strictPort: true,
+port: 8000,
+},
+resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
 });
